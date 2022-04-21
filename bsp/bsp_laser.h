2da7404a8c685e9a91e3c6d6a729c5a75860bd22
Cjsah <2684117397@qq.com>
2022-04-21 12:14:03 +0800

    init

---
+++ bsp/bsp_laser.h
@@ -0,0 +1,7 @@
+#ifndef BSP_LASER_H
+#define BSP_LASER_H
+
+extern void laser_on(void);
+extern void laser_off(void);
+
+#endif
---
+++ bsp/bsp_led.h
@@ -0,0 +1,9 @@
+#ifndef BSP_LED_H
+#define BSP_LED_H
+
+extern void led_off(void);
+extern void led_r_on(void);
+extern void led_g_on(void);
+extern void led_b_on(void);
+
+#endif
